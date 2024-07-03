4c5d1a8077376b66d68e83341f5acf17e146387b
Mariana <mariianaf3@gmail.com>
2024-07-03 13:20:48 -0400

    Modificaciones de autentificacion

--- mce/static/mce/js/validarRegistro.js
+++ mce/static/mce/js/validarRegistro.js
@@ -1,91 +1,90 @@
 $(document).ready(function () {
-    $('#form-register').submit(function (e) {
-        e.preventDefault();
-        let passwordRegister = $("#pass").val();
-        let verifyPassword = $("#verifyPass").val();
-        let user = $('#usuarioRegistro').val();
-        let userTrim = $.trim(user);
+    $('#form-register').on('submit', function (event) {
+        let isValid = true;
+        $('.error').hide();
 
+        let username = $('#username').val().trim();
         let codCountry = $('#codPais').val();
         let phone = $('#phone').val();
-
-        if (userTrim.length < 5 || userTrim.length > 20) {
-            $('#messageUsuario').text('Ingresa tu nombre de usuario (debe tener entre 5 y 20 caracteres)').fadeIn();
-            return false;
-        } else if (userTrim.indexOf(' ') >= 0) {
-            $('#messageUsuario').text('El nombre de usuario no puede contener espacios').fadeIn();
-            return false;
+        if (username === '') {
+            isValid = false;
+            $('#messageUsuario').text('Ingrese un nombre de usuario válido').fadeIn();
+        } else if (username.length < 5 || username.length > 20) {
+            isValid = false;
+            $('#messageUsuario').text('El nombre de usuario debe tener entre 5 y 20 caracteres').fadeIn();
         } else {
             $('#messageUsuario').fadeOut();
         }
 
-        let email = $('#emailRegistro').val();
-
-        if (email == '') {
-            $('#messageEmail').text('El campo de correo no puede estar vacío').fadeIn();
-            return false;
-        } else if (email.indexOf(' ') >= 0) {
-            $('#messageEmail').text('El correo electrónico no puede contener espacios').fadeIn();
-            return false;
+        let email = $('#email').val().trim();
+        if (email === '') {
+            isValid = false;
+            $('#messageEmail').text('Ingrese un correo electrónico válido').fadeIn();
         } else if (!isEmail(email)) {
-            $('#messageEmail').text('El correo electrónico no es válido, ocupa @ y dominio').fadeIn();
-            return false;
+            isValid = false;
+            $('#messageEmail').text('Formato de correo electrónico incorrecto').fadeIn();
         } else {
             $('#messageEmail').fadeOut();
         }
 
         if (phone !== "" && codCountry === "+56" && !isChile(phone)) {
+            isValid = false;
             $('#messagePhone').text('El número móvil de Chile comienza con 9').fadeIn();
-            return false;
         } else {
             $('#messagePhone').fadeOut();
         }
 
-        if (passwordRegister == '') {
-            $('#messagePassword').text('La contraseña no puede estar vacía').fadeIn();
-            return false;
+        let passwordRegister = $('#pass').val().trim();
+        if (passwordRegister === '') {
+            isValid = false;
+            $('#messagePassword').text('Ingrese una contraseña válida').fadeIn();
         } else if (!isPass(passwordRegister)) {
-            $('#messagePassword').text('La contraseña debe tener al menos una mayúscula, una minúscula, un número y un carácter especial(. / - @)').fadeIn();
-            return false;
+            isValid = false;
+            $('#messagePassword').text('La contraseña debe tener al menos una mayúscula, una minúscula, un número y un carácter especial (. / @)').fadeIn();
         } else {
             $('#messagePassword').fadeOut();
         }
 
-        if (verifyPassword == '') {
-            $('#messageConfirm').text('Confirma la contraseña').fadeIn();
-            return false;
+        let verifyPassword = $('#verifyPass').val().trim();
+        if (verifyPassword === '') {
+            isValid = false;
+            $('#messageConfirm').text('Confirme la contraseña').fadeIn();
         } else if (verifyPassword !== passwordRegister) {
-            $('#messageConfirm').text('Las contraseñas deben coincidir').fadeIn();
-            return false;
+            isValid = false;
+            $('#messageConfirm').text('Las contraseñas no coinciden').fadeIn();
         } else {
             $('#messageConfirm').fadeOut();
         }
 
         if (!$('#formCheck').prop('checked')) {
+            isValid = false;
             $('#checkeado').text('Acepta los términos y condiciones para continuar').fadeIn();
-            return false;
         } else {
             $('#checkeado').fadeOut();
         }
 
-        // Alerta Sweet
-        Swal.fire({
-            title: 'Registro Exitoso',
-            text: '¡Tu registro ha sido completado con éxito!',
-            icon: 'success',
-            confirmButtonText: 'Iniciar Sesión',
-            timer: 6000
-        }).then((result) => {
-            if (result.isConfirmed) {
-                window.location.href = '../mce/login';
-            } else {
-                $('form').unbind('submit').submit();
-            }
-        });
+        if (!isValid) {
+            event.preventDefault();
+        } else {
+
+            Swal.fire({
+                title: 'Registro Exitoso',
+                text: '¡Tu registro ha sido completado con éxito!',
+                icon: 'success',
+                confirmButtonText: 'Iniciar Sesión',
+                timer: 6000
+            }).then((result) => {
+                if (result.isConfirmed) {
+                    window.location.href = '../mce/loginC';
+                } else {
+                    $('#form-register').off('submit').submit();
+                }
+            });
+        }
     });
 
     function isEmail(emailTrim) {
-        const emailRegex = /^[a-zA-Z0-9_.-]+@[a-zA-Z0-9]+\.[a-zA-Z0-9]+$/;
+        const emailRegex = /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/;
         return emailRegex.test(emailTrim);
     }
 
@@ -102,7 +101,7 @@ $(document).ready(function () {
     $('#phone').on('input', function () {
         let phone = $(this).val();
         if (phone.length > 9) {
-          $(this).val(phone.substr(0, 9));
+            $(this).val(phone.substr(0, 9));
         }
-      });
+    });
 });
