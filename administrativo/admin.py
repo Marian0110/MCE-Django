f1610a498989300908ed4160b8f62ed2eb3ce516
Mariana <mariianaf3@gmail.com>
2024-07-04 17:05:28 -0400

    Añadiendo modelo pais a panel administrativo

--- administrativo/admin.py
+++ administrativo/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from mce.models import Cliente, Producto, Accesorio, Historia, Nosotros
+from mce.models import Cliente, Producto, Accesorio, Historia, Nosotros, Pais
 # Register your models here.
 
 admin.site.register(Cliente)
@@ -7,3 +7,4 @@ admin.site.register(Producto)
 admin.site.register(Accesorio)
 admin.site.register(Historia)
 admin.site.register(Nosotros)
+admin.site.register(Pais)
